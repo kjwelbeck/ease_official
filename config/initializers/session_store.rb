e2244af2274e47bf661615795695a84f2d4387d9
Kojo Welbeck <kojo.welbeck99@gmail.com>
2013-07-13 18:24:03 -0400

    Initial Commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+EaseOfficial::Application.config.session_store :cookie_store, key: '_ease_official_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# EaseOfficial::Application.config.session_store :active_record_store
